2f7a85529a5c7a7ff4adf2f6c082e328b1a46ef6
rere <regis-graptin@sfr.fr>
2025-05-30 21:51:07 +0200

    feat: add rainbowkit wallet

---
+++ front/src/utils/wagmi.ts
@@ -0,0 +1,15 @@
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { http } from "wagmi";
+import { optimismSepolia } from "wagmi/chains";
+
+export const config = getDefaultConfig({
+  appName: process.env.NEXT_PUBLIC_SITE_NAME!,
+  projectId: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID!,
+  chains: [optimismSepolia],
+  transports: {
+    [optimismSepolia.id]: http(
+      `https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`
+    ),
+  },
+  ssr: true,
+});
