6fccf714d7a1575b4d59cf5aa3d81bc66363e68d
rere <regis-graptin@sfr.fr>
2025-05-31 01:53:21 +0200

    fix: wagmi issue - incorrect rpc url

--- front/src/utils/wagmi.ts
+++ front/src/utils/wagmi.ts
@@ -8,7 +8,7 @@ export const config = getDefaultConfig({
   chains: [optimismSepolia],
   transports: {
     [optimismSepolia.id]: http(
-      `https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`
+      `https://opt-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`
     ),
   },
   ssr: true,
